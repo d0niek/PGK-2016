415d9f24ef8462ce63fd339b3a55fe8ec66276dd
d0niek <damianglinkowski@gmail.com>
2016-10-19 17:49:37 +0200

    Added simply player with movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+	public float speed = 6.0F;
+	public float jumpSpeed = 8.0F;
+	public float gravity = 20.0F;
+	private Vector3 moveDirection = Vector3.zero;
+
+	private CharacterController cc;
+
+	void Start ()
+	{
+		cc = GetComponent<CharacterController>();
+	}
+
+	void FixedUpdate ()
+	{
+		if (cc.isGrounded) {
+			float moveHorizontal = Input.GetAxis ("Horizontal");
+			float moveVertical = Input.GetAxis ("Vertical");
+
+			moveDirection = new Vector3(moveHorizontal, 0, moveVertical);
+			moveDirection = transform.TransformDirection(moveDirection);
+			moveDirection *= speed;
+			if (Input.GetButton ("Jump")) {
+				moveDirection.y = jumpSpeed;
+			}
+		}
+
+		moveDirection.y -= gravity * Time.deltaTime;
+		cc.Move(moveDirection * Time.deltaTime);
+	}
+}
