89b042947ed3c076cd025e90f4f9a1c1516f4cc7
d0niek <damianglinkowski@gmail.com>
2016-11-15 09:37:20 +0100

    Enemies follow and attack player if in range

---
+++ Assets/Scripts/Enemy/EnemyHealth.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+	public int startingHealth = 100;          
+	public int currentHealth;                        
+	public int scoreValue = 10;         
+
+	CapsuleCollider capsuleCollider;        
+	bool isDead;                                                 
+
+	void Awake ()
+	{
+		capsuleCollider = GetComponent <CapsuleCollider> ();
+		currentHealth = startingHealth;
+	}
+
+	void Update ()
+	{
+	}
+
+	public void TakeDamage (int amount, Vector3 hitPoint)
+	{
+		if (isDead) {
+			return;
+		}
+
+		currentHealth -= amount;
+
+		if (currentHealth <= 0) {
+			Death ();
+		}
+	}
+		
+	void Death ()
+	{
+		isDead = true;
+
+		// Turn the collider into a trigger so shots can pass through it.
+		capsuleCollider.isTrigger = true;
+	}
+
+	public void StartSinking ()
+	{
+		// Find and disable the Nav Mesh Agent.
+		GetComponent <NavMeshAgent> ().enabled = false;
+
+		// Find the rigidbody component and make it kinematic (since we use Translate to sink the enemy).
+		GetComponent <Rigidbody> ().isKinematic = true;
+
+		// Increase the score by the enemy's score value.
+		//ScoreManager.score += scoreValue;
+
+		Destroy (gameObject, 2f);
+	}
+}
---
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMovement : MonoBehaviour
+{
+	public float range = 10.0f;
+
+	float eps = 0.000001f;
+	GameObject player;   
+	PlayerHealth playerHealth;
+	NavMeshAgent navMeshAgent;
+	Vector3 startPosition;
+
+	void Start ()
+	{
+		player = GameObject.FindGameObjectWithTag ("Player");
+		playerHealth = player.GetComponent <PlayerHealth> ();
+		navMeshAgent = GetComponent<NavMeshAgent> ();
+		startPosition = transform.position;
+	}
+
+	void Update ()
+	{
+		if (InRange () && playerHealth.currentHealth > 0) {
+			navMeshAgent.SetDestination (player.transform.position);
+		} else if (startPosition != transform.position) {
+			navMeshAgent.SetDestination (startPosition);
+		}
+	}
+
+	bool InRange()
+	{
+		float distance = Vector3.Distance (transform.position, player.transform.position);
+		distance = Mathf.Abs (distance);
+		print (distance);
+
+		return distance <= range;
+	}
+}
