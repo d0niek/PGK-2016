8b5fe06231749576864c831d367dfeb982b195da
d0niek <damianglinkowski@gmail.com>
2016-11-22 11:34:59 +0100

    Range of hearing and view

--- Assets/Scripts/Enemy/EnemyMovement.cs
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -3,13 +3,16 @@ using System.Collections;
 
 public class EnemyMovement : MonoBehaviour
 {
-	public float hearingRange = 10.0f;
+	public float hearingRange = 5.0f;
 	public GameObject hearingRangeCircle;
+	public float viewDistance = 10.0f;
+	public float viewAngle = 30.0f;
 
 	GameObject player;
 	PlayerHealth playerHealth;
 	NavMeshAgent navMeshAgent;
 	Vector3 startPosition;
+	bool PlayerInRange;
 
 	void Start ()
 	{
@@ -18,16 +21,24 @@ public class EnemyMovement : MonoBehaviour
 		navMeshAgent = GetComponent<NavMeshAgent> ();
 		startPosition = transform.position;
 		hearingRangeCircle.transform.localScale = new Vector3 (2 * hearingRange, 0.0001f, 2 * hearingRange);
+		PlayerInRange = false;
 	}
 
 	void Update ()
 	{
-		if (InHearingRange () && playerHealth.currentHealth > 0) {
+		if ((InHearingRange () || InViewRange ()) && playerHealth.currentHealth > 0) {
 			navMeshAgent.SetDestination (player.transform.position);
 		} else if (startPosition != transform.position) {
 			navMeshAgent.SetDestination (startPosition);
 		}
 	}
+
+//	void FixedUpdate ()
+//	{
+//		bool inView = InViewRange ();
+//		Debug.Log ("In view range " + inView);
+//		PlayerInRange = InHearingRange () || InViewRange ();
+//	}
 
 	bool InHearingRange ()
 	{
@@ -35,5 +46,20 @@ public class EnemyMovement : MonoBehaviour
 		distance = Mathf.Abs (distance);
 
 		return distance <= hearingRange;
+	}
+
+	bool InViewRange()
+	{
+		RaycastHit hit;
+		Vector3 rayDirection = (player.transform.position - transform.position).normalized;
+
+		if (Vector3.Angle (transform.forward, rayDirection) < viewAngle / 2) {
+			if (Physics.Raycast (transform.position + transform.forward, rayDirection, out hit, viewDistance)) {
+				Debug.Log (hit.transform.name + ", " + hit.transform.tag);
+				return hit.transform.tag == "Player";
+			}
+		}
+
+		return false;
 	}
 }
