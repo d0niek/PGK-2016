32375f41ebb4b4f61686d3489e9235c079c1ac2e
d0niek <damianglinkowski@gmail.com>
2017-01-12 21:16:25 +0100

    Stun enemies

--- Assets/Scripts/Follower/explosionController.cs
+++ Assets/Scripts/Follower/explosionController.cs
@@ -13,9 +13,13 @@ public class explosionController : MonoBehaviour {
 
     void OnParticleCollision (GameObject other)
     {
-        if (other.gameObject.CompareTag("Box"))
-        {
+        if (other.gameObject.CompareTag("Box")) {
             other.gameObject.SetActive(false);
         }
+
+		if (other.gameObject.CompareTag("Enemy")) {
+			EnemyMovement em = other.gameObject.GetComponent <EnemyMovement> ();
+			em.Stun ();
+		}
     }
 }
