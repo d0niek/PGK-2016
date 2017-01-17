fa7a1abf849de8d4d26e93ac7316abd1c4e63678
Leonard Oganiesian <187203@edu.p.lodz.pl>
2017-01-17 11:38:45 +0100

    Naprawione ogluszanie, dodane klucze

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -5,13 +5,18 @@ using System.Collections;
 public class PlayerController : MonoBehaviour
 {
 	public Text goldCountText;
+    public Text keysCountText;
 
 	private int goldCount;
+    private int keyCount;
+
 
 	void Awake ()
 	{
 		goldCount = 0;
+        keyCount = 0;
 		SetGoldCountText ();
+        SetKeysCountText ();
 	}
 
 	void OnTriggerEnter (Collider other)
@@ -22,10 +27,22 @@ public class PlayerController : MonoBehaviour
 			goldCount++;
 			SetGoldCountText ();
 		}
+
+        if (other.gameObject.CompareTag("Key"))
+        {
+            other.gameObject.SetActive(false);
+            keyCount++;
+            SetKeysCountText();
+        }
     }
+
 
 	void SetGoldCountText ()
 	{
 		goldCountText.text = "Gold: " + goldCount.ToString ();
 	}
+    void SetKeysCountText()
+    {
+        keysCountText.text = "Keys: " + keyCount.ToString();
+    }
 }
