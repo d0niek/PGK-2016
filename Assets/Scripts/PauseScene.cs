aef2aebad616c6fa3830c1fbf29194f90df5c985
Leonard Oganiesian <187203@edu.p.lodz.pl>
2017-01-21 01:23:28 +0100

    Funkcjonalność pauzy

---
+++ Assets/Scripts/PauseScene.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseScene : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void Resume ()
+    {
+        Time.timeScale = 1;
+        Destroy(gameObject);
+        Debug.Log("DESTROYED");
+    }
+}
